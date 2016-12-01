1eb3de6ef9423c8dd879cd4c6b13bf32cd2689cd
r-okinaga <Hrnn0PDk1>
2016-12-01 16:19:35 +0900

    cap設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,36 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'cockpit'
+set :repo_url, 'git@github.com:r-okinaga/cockpit.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :deploy_to, "/var/www/rails/#{fetch(:application)}"
+set :log_Level, :info
+set :linked_dirs, %w(log public/assets public/system tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle)
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+set :rbenv_path, '~/.rbenv'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
-# Default value for :scm is :git
-# set :scm, :git
+SSHKit.config.command_map[:rake] = 'bundle exec rake'
+SSHKit.config.command_map[:rails] = 'bundle exec rails'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+namespace :deploy do
+    task :restart do
+        on roles(:app), in: :sequence, wait: 5 do
+            execute :touch, release_path.join('tmp/restart.txt')
+        end
+    end
+    task :db_seed do
+        on roles(:db) do |host|
+            within current_path do
+                with rails_env: fetch(:rails_env) do
+                    execute :rake, 'db:seed'
+                end
+            end
+        end
+    end
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+    after :finishing, 'deploy:cleanup'
+end
