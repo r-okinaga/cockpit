cca89e82c9bce4fa5b9168be92335c8468baf0fe
r-okinaga <Hrnn0PDk1>
2016-12-01 16:32:27 +0900

    cap設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :branch, 'master'
 set :rails_env, 'production'
 set :keep_releases, 5
 
-server '192.168.0.137', roles: %w(web app db), user: 'root'
+server '192.168.0.137', roles: %w(web app db), user: 'rails'
