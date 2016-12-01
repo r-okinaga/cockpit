1305f7cfda86b1abd4fb0eb46c530de8a8d82286
r-okinaga <Hrnn0PDk1>
2016-12-01 19:54:44 +0900

    api実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
     root 'rowdata#index'
 
+    namespace :api, {format: 'json'} do
+        controller 'rowdata' do
+          post 'create'
+        end
+    end
 end
