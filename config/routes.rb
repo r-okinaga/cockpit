d75d55f4b882c2e1df90ee6bdf1529fa2799f532
r-okinaga <Hrnn0PDk1>
2016-12-21 19:02:44 +0900

    スプレッドシートID受け取り口作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
     root 'rowdata#index'
-    get 'rowdata/show', to: 'rowdata#show'
+    post 'rowdata/show', to: 'rowdata#show'
     post 'rowdata/create', to: 'rowdata#create'
 
    namespace :api, {format: 'json'} do
